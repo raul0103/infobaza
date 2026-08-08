c85dc8c9ddb6c7b81a5468b080ab93c43239bad6
raul0103 <rshakurov95@mail.ru>
2026-08-08 12:06:08 +0500

    upd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
         <a href="{{ $addQuoteUrl }}" class="btn btn-secondary">+ Цитата</a>
         <a href="{{ $addWordUrl }}" class="btn btn-primary">
             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/></svg>
-            + Слово
+            Слово
         </a>
     </x-slot:actions>
 </x-page-header>
